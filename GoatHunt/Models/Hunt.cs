129c989c160ea620fea25ae3520fbb8d99f8438d
ksindorf000 <ksindorf000@gmail.com>
2017-02-21 13:46:42 -0500

    Initial

---
+++ GoatHunt/Models/Hunt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoatHunt.Models
+{
+    public class Hunt
+    {
+        [Key]
+        public int HuntNumber { get; set; }
+        public string HuntName { get; set; }
+        public string Type { get; set; }
+        public string Permits { get; set; }
+        public string NumHuntersAfield { get; set; }
+        public string Harvest { get; set; }
+        public string AverageDays { get; set; }
+        public string PercentSuccess { get; set; }
+    }
+}
